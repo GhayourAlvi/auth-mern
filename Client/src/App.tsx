911a295b9b6a560357b57307d66097ae4b1770eb
GhayourAlvi <alvighayour110@gmail.com>
2023-12-27 19:32:11 +0500

    add server

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,8 +3,10 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import About from "./pages/About"
+import Header from "./components/Header"
 export default function App() {
   return <BrowserRouter>
+  <Header/>
     <Routes>
       <Route path="/" element={<Home/> }/>
       <Route path="/login" element={<Login/> }/>
