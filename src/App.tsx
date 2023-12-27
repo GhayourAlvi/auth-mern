a6d639989c1b642d2d11c84a2589509b4c3778ee
GhayourAlvi <alvighayour110@gmail.com>
2023-12-27 17:36:47 +0500

    add routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
 export default function App() {
-  return (
-    <div>
-      <h1>Auth Client</h1>
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/> }/>
+      <Route path="/login" element={<Login/> }/>
+      <Route path="/register" element={<SignUp/> }/>
+      <Route path="about" element={<About/>}/>
+    </Routes>
+  </BrowserRouter>
 }
