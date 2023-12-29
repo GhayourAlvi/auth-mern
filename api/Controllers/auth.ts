0573fe0b0df0442912856a008357cd07ca826607
GhayourAlvi <alvighayour110@gmail.com>
2023-12-29 18:52:00 +0500

    add middleware

--- api/Controllers/auth.ts
+++ api/Controllers/auth.ts
@@ -1,17 +1,15 @@
 import User from "../Models/user";
 import bcryptjs from "bcryptjs"
-export const signUp = async (req: any, res: any) => {
-
+export const signUp = async (req: any, res: any, next: any) => {
     const { firstName, lastName, email, password } = req.body;
     const hashedPassword=bcryptjs.hashSync(password,10)
     const user = new User({ firstName, lastName, email, password: hashedPassword});
- 
     try {
         await user.save();
         res.status(201).json({
             message: "Sign Up Successful",
         });
     } catch (error: any) {
-        res.status(500).json(error.message);
+        next(error)
     }
 }
--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import connectToMongo from "./db";
 import dotenv from "dotenv";
 import userRoute from "./Routes/user"
 import authRoute from "./Routes/auth"
+import { ErrorResponse } from "./Interfaces/ErrorResponse";
 
 dotenv.config()
 connectToMongo();
@@ -18,6 +19,19 @@ app.listen(3000, () => {
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
+
+// Middleware for handling errors
+app.use((err: any, req: any, res: any, next: any) => {
+  const statusCode: number = err.statusCode || 500;
+  const message: string = err.message || 'Internal Server Error';
+
+  const errorResponse: ErrorResponse = {
+    success: false,
+    message,
+    statusCode,
+  };
+  res.status(statusCode).json(errorResponse);
+});
 
 
 
